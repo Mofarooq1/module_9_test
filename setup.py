105f73e9beb8f7c9d716e1bddd8fb0431f4891b3
Mohammad Farooq <mbf4ay@virgini.edu>
2023-10-24 20:20:40 -0400

    changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'module9test',
+    version = '0.1',
+    author = 'Mohammad Farooq',
+    author_email = 'mbf4ay@virginia.com',
+    packages = ['module9test'],
+    url = 'https://github.com/Mofarooq1/module_9_test',
+    license = 'MIT',
+    description = 'A simple module 9 test',
+)
